2222e2f8b7f4db1e59fd9544c03229ded72e4c52
Dominic Tarr <dominic.tarr@gmail.com>
2013-07-01 21:50:56 -0700

    accept tmp config

--- index.js
+++ index.js
@@ -11,118 +11,132 @@ var tar     = require('tar')
 var pull    = require('pull-stream')
 var pt      = require('pull-traverse')
 
-var registry = 'http://registry.npmjs.org'
 
-function getUrl (name, ver) {
-  return registry +"/" + name + "/-/" + name + "-" + ver + ".tgz"
-}
+module.exports = function (config) {
+  var exports = installTree
+  //FIX THIS
+  var registry = config.registry || 'http://registry.npmjs.org'
 
-function empty (obj) {
-  for(var i in obj)
-    return false
-  return true
-}
+  var tmpdir = config.tmp || os.tmpdir()
 
-var preparePackage = exports.preparePackage =
-function (pkg, cb) {
-  if(!pkg.version)
-    return cb(new Error(pkg.name + ' has no version'))
+  function getUrl (name, ver) {
+    return registry +"/" + name + "/-/" + name + "-" + ver + ".tgz"
+  }
 
-  var name = pkg.name
-  var ver  = pkg.version
-  var cache = path.join(process.env.HOME, '.npm', name, ver, 'package.tgz')
-  var tmp = path.join(os.tmpdir(), ''+Date.now() + Math.random())
+  function empty (obj) {
+    for(var i in obj)
+      return false
+    return true
+  }
 
-  fs.stat(cache, function (err) {
+  var preparePackage = exports.preparePackage =
+  function (pkg, cb) {
+    if(!pkg.version)
+      return cb(new Error(pkg.name + ' has no version'))
 
-    var createStream = !err
-    ? function (cb) { cb(null, fs.createReadStream(cache)) }
-    : function (cb) {
-      mkdirp(path.dirname(cache), function () {
-        http.get(getUrl(name, ver), function (res) {      
-          res.pipe(fs.createWriteStream(cache))
-          cb(null, res)
-        })
-      })
-    }
+    var name = pkg.name
+    var ver  = pkg.version
+    //SHOULD COME FROM CONFIG
+    var cache = path.join(process.env.HOME, '.npm', name, ver, 'package.tgz')
+    var tmp = path.join(tmpdir, ''+Date.now() + Math.random())
 
-    mkdirp(tmp, function (err) {
-      if(err) return cb(err)
-      createStream(function (err, stream) {
-        var i = 1
-        stream.on('error', next)
-        stream.pipe(zlib.createGunzip())
-        .pipe(tar.Extract({path: tmp}))
-        .on('end', next)
-        function next (err) {
-          if(--i) return
-          cb(err, tmp)
-        }
-      })
-    })
+    fs.stat(cache, function (err) {
 
-  })
-}
-
-var installTree = exports.installTree =
-function (tree, opts, cb) {
-  if(!cb)
-    cb = opts, opts = {}
-
-  tree.path = path.join(opts.path || process.cwd(), 'node_modules')
-
-  pull(
-    pt.widthFirst(tree, function (pkg) {
-      return pull(
-        pull.values(pkg.dependencies),
-        pull.map(function (_pkg) {
-          _pkg.path = path.join(pkg.path, pkg.name, 'node_modules')
-          return _pkg
-        })
-      )
-    }),
-    //unpack every file, so that it can be moved into place.
-    //optimization: if a module has no deps,
-    //just link it.
-    pull.asyncMap(function (pkg, cb) {
-      preparePackage(pkg, function (err, data) {
-        pkg.tmp = data
-        cb(err, pkg)
-      })
-    }, 64),
-    pull.asyncMap(function (pkg, cb) {
-      if(!pkg.tmp)
-        return cb(new Error('no path for:'+ pkg.name))
-
-      var source = path.join(pkg.tmp, 'package')
-      var dest   = path.join(pkg.path, pkg.name)
-      mkdirp(pkg.path, function () {
-        fs.lstat(dest, function (err) {
-          if(!err) return cb()
-          fs.rename(source, dest, function (err) {
-            console.error(pkg.name + '@' + pkg.version, '->', pkg.path)
-            cb(err)
+      var createStream = !err
+      ? function (cb) { cb(null, fs.createReadStream(cache)) }
+      : function (cb) {
+        mkdirp(path.dirname(cache), function () {
+          http.get(getUrl(name, ver), function (res) {      
+            res.pipe(fs.createWriteStream(cache))
+            cb(null, res)
           })
         })
+      }
+
+      mkdirp(tmp, function (err) {
+        if(err) return cb(err)
+        createStream(function (err, stream) {
+          var i = 1
+          stream.on('error', next)
+          stream.pipe(zlib.createGunzip())
+          .pipe(tar.Extract({path: tmp}))
+          .on('end', next)
+          function next (err) {
+            if(--i) return
+            cb(err, tmp)
+          }
+        })
       })
-    }),
-    pull.drain(null, cb)
-  )
 
-}
-
-exports = module.exports = installTree
-
-exports.commands = function (db, config) {
-  db.cli.push(function (db, config, cb) {
-    var args = config._.slice()
-    if('install' !== args.shift()) return
-    db.resolve(args, {greedy: config.greedy}, function (err, tree) {
-      if(err) return cb(err)
-      installTree(tree, {path: config.installPath}, cb)
     })
   }
-}
 
-exports.db = function () {}
+  exports.installTree = installTree
+  function installTree (tree, opts, cb) {
+    if(!cb)
+      cb = opts, opts = {}
+
+    tree.path = path.join(opts.path || process.cwd(), 'node_modules')
+
+    pull(
+      pt.widthFirst(tree, function (pkg) {
+        return pull(
+          pull.values(pkg.dependencies),
+          pull.map(function (_pkg) {
+            _pkg.path = path.join(pkg.path, pkg.name, 'node_modules')
+            return _pkg
+          })
+        )
+      }),
+      //unpack every file, so that it can be moved into place.
+      //optimization: if a module has no deps,
+      //just link it.
+      pull.asyncMap(function (pkg, cb) {
+        preparePackage(pkg, function (err, data) {
+          pkg.tmp = data
+          cb(err, pkg)
+        })
+      }, 64),
+      pull.asyncMap(function (pkg, cb) {
+        if(!pkg.tmp)
+          return cb(new Error('no path for:'+ pkg.name))
+
+        var source = path.join(pkg.tmp, 'package')
+        var dest   = path.join(pkg.path, pkg.name)
+        mkdirp(pkg.path, function () {
+          fs.lstat(dest, function (err) {
+            if(!err) return cb()
+            fs.rename(source, dest, function (err) {
+              console.error(pkg.name + '@' + pkg.version, '->', pkg.path)
+              cb(err)
+            })
+          })
+        })
+      }),
+      pull.drain(null, cb)
+    )
+
+  }
+
+  //exports = module.exports = installTree
+
+  exports.commands = function (db, config) {
+    db.commands.push(function (db, config, cb) {
+      var args = config._.slice()
+      if('install' !== args.shift()) return
+
+      db.resolve(args.shift(), config, function (err, tree) {
+        if(err) return cb(err)
+        installTree(tree, {
+          path: config.installPath, tmp: config.tmp
+        }, cb)
+      })
+      return true
+    })
+  }
+
+  exports.db = function () {}
+
+  return exports
+}
 
