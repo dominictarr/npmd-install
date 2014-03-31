81e48130a16b6fc00da540ccb3450049d4d8c174
Dominic Tarr <dominic.tarr@gmail.com>
2014-03-31 16:59:48 +1300

    injectable npm-cache

--- index.js
+++ index.js
@@ -18,34 +18,6 @@ var clone   = require('clone')
 var tarfs   = require('tar-fs')
 var zlib    = require('zlib')
 
-var get = require('npmd-cache')({path: path.join(process.env.HOME, '.npmd')})
-
-function unpack (pkg, opts, cb) {
-  var start = Date.now()
-
-  //get from the hash if it's already been downloaded!
-  //else download from the registry or the url.
-  var query = {
-    key: /\//.test(pkg.from) ? pkg.from : pkg.name + '@' + pkg.version,
-    hash: pkg.shasum
-  }
-
-  return get.createStream(query, function (err, stream) {
-    if(err) return cb(err)
-    if(!stream) throw new Error('did not return stream')
-    var hash = crypto.createHash(opts.alg || 'sha1')
-
-    stream
-      .on('data', function (d) { hash.update(d) })
-      .on('error', cb)
-      .pipe(zlib.createGunzip())
-      .pipe(tarfs.extract(opts.target))
-      .on('finish', function () {
-        cb(null, hash.digest('hex'))
-      })
-  })
-}
-
 
 var EventEmitter = require('events').EventEmitter
 
@@ -82,44 +54,6 @@ function randDir (pre) {
   return path.join(tmpdir, (pre || '') + Date.now() + '-' + Math.random())
 }
 
-var installTree = cont.to(function(tree, opts, cb) {
-  if(!cb)
-    cb = opts, opts = {}
-
-  //this only works on unix. in practice, you must pass in the config object.
-  var cache = opts.cache || path.join(process.env.HOME, '.npm')
-
-  var installPath = opts.path || process.cwd()
-
-  tree.path = path.join(installPath, 'node_modules')
-
-  pull(
-    pt.widthFirst(tree, function (pkg) {
-      if(!pkg.dependencies)
-        return emptyStream()
-      
-      return pull(
-        pull.values(pkg.dependencies),
-        pull.map(function (_pkg) {
-          _pkg.path = path.join(pkg.path, pkg.name, 'node_modules')
-          return _pkg
-        })
-      )
-    }),
-    paramap(function (pkg, cb) {
-      unpack(pkg, {target: pkg.path, alg: config.alg}, function (err, hash) {
-          if(hash !== pkg.shasum) return cb(new Error(
-            'expected ' + pkg.name +'@' + pkg.version +'\n' +
-            'to have shasum=' + pkg.shasum + ' but was='+hash))
-          cb(err, pkg)
-      })
-    }, 32),
-    pull.drain(null, cb)
-  )
-
-})
-
-
 function clean(tree) {
 
   function _clean (tree) {
@@ -141,25 +75,94 @@ function clean(tree) {
 }
 
 
-var install = exports =  module.exports =
-cont.to(function (tree, opts, cb) {
-  if(!cb) cb = opts, opts = {}
 
-  if('string' === typeof tree.name)
-    return installTree(tree, opts) (next)
+var inject = module.exports = function (cache) {
 
-  cpara(map(tree, function (tree) {
-    return installTree(tree, opts)
-  })) (next)
+  function unpack (pkg, opts, cb) {
+    var start = Date.now()
 
-  function next (err) {
-    cb(err, clean(clone(tree)))
+    //get from the hash if it's already been downloaded!
+    //else download from the registry or the url.
+    var query = {
+      key: /\//.test(pkg.from) ? pkg.from : pkg.name + '@' + pkg.version,
+      hash: pkg.shasum
+    }
+
+    return cache.createStream(query, function (err, stream) {
+      if(err) return cb(err)
+      if(!stream) throw new Error('did not return stream')
+      var hash = crypto.createHash(opts.alg || 'sha1')
+
+      stream
+        .on('data', function (d) { hash.update(d) })
+        .on('error', cb)
+        .pipe(zlib.createGunzip())
+        .pipe(tarfs.extract(opts.target))
+        .on('finish', function () {
+          cb(null, hash.digest('hex'))
+        })
+    })
   }
-})
+
+  var installTree = cont.to(function(tree, opts, cb) {
+    if(!cb)
+      cb = opts, opts = {}
+
+    //this only works on unix. in practice, you must pass in the config object.
+    var cache = opts.cache || path.join(process.env.HOME, '.npm')
+
+    var installPath = opts.path || process.cwd()
+
+    tree.path = path.join(installPath, 'node_modules')
+
+    pull(
+      pt.widthFirst(tree, function (pkg) {
+        if(!pkg.dependencies)
+          return emptyStream()
+      
+        return pull(
+          pull.values(pkg.dependencies),
+          pull.map(function (_pkg) {
+            _pkg.path = path.join(pkg.path, pkg.name, 'node_modules')
+            return _pkg
+          })
+        )
+      }),
+      paramap(function (pkg, cb) {
+        unpack(pkg, {target: pkg.path, alg: config.alg}, function (err, hash) {
+            if(hash !== pkg.shasum) return cb(new Error(
+              'expected ' + pkg.name +'@' + pkg.version +'\n' +
+              'to have shasum=' + pkg.shasum + ' but was='+hash))
+            cb(err, pkg)
+        })
+      }, 32),
+      pull.drain(null, cb)
+    )
+
+  })
+
+  return cont.to(function (tree, opts, cb) {
+    if(!cb) cb = opts, opts = {}
+
+    if('string' === typeof tree.name)
+      return installTree(tree, opts) (next)
+
+    cpara(map(tree, function (tree) {
+      return installTree(tree, opts)
+    })) (next)
+
+    function next (err) {
+      cb(err, clean(clone(tree)))
+    }
+  })
+
+}
 
 //process.on is test for !browserify
 if(!module.parent && process.on) {
   var config = require('npmd-config')
+  var cache = require('npmd-cache')(config)
+  var install = inject(cache)
 
   if(config.version) {
     console.log(require('./package').version)
